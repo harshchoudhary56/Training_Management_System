6b69b2b3e817ba38e669e3270e6dd0019a8bfbf4
Harsh Choudhary <hc123656@gmail.com>
2025-09-05 22:58:55 +0530

    initial configuration

---
+++ src/test/java/com/ec_infosolutions/training_management_system/TrainingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec_infosolutions.training_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
